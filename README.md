e951ffc65cdd5ed4d7b0ae46940676885017d214
Leftium <john@leftium.com>
2020-12-21 01:26:01 +0900

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# svelte-coffeescript-pug
+
+Modified [svelte app template](https://github.com/sveltejs/template):
+
+- Support for CoffeeScript and Pug.
+- Added .gitattributes file.
+- Converted tabs to spaces. 
+
+Read the [original documentation](https://github.com/sveltejs/template/blob/929052a297a4ee4b5c1a967fa9956328a0c1eb71/README.md) for the template this project is based on.
---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script type='text/coffeescript'>
+    export name = null
+    console.log 'Hello Svelte-CoffeeScript!'
+</script>
+
+<template lang=pug>
+main
+    h1 Hello {name}!
+    p.
+        Visit the #[a(href="https://svelte.dev/tutorial") Svelte tutorial]
+        to learn how to build Svelte apps.
+</template>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
